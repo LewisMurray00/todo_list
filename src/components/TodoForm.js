7217596710daa8d31236d160c0ab9627f272aea9
Lewis Murray <lewisdmurray00@gmail.com>
2021-10-11 18:33:36 +0100

    Styled the input form and button when being edited

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -27,7 +27,7 @@ function TodoForm(props) {
     return (
         <form className="todo-form" onSubmit={handleSubmit}>
             <input type='text' placeholder='Add a task!' value={input} name='text' className='todo-input' onChange={handleChange} ref={inputRef} />
-        <button className='todo-button'>Add a task</button>
+        <button className='todo-button'>Add</button>
         </form>
     )
 }
