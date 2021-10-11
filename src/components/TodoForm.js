210b2cdc6326acd22a8dc6e82644bb7f7aab4143
Lewis Murray <lewisdmurray00@gmail.com>
2021-10-11 18:29:22 +0100

    styled the add button

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -26,8 +26,8 @@ function TodoForm(props) {
 
     return (
         <form className="todo-form" onSubmit={handleSubmit}>
-            <input type='text' placeholder='Add a todo' value={input} name='text' className='todo-input' onChange={handleChange} ref={inputRef} />
-        <button className='todo-button'>Add todo</button>
+            <input type='text' placeholder='Add a task!' value={input} name='text' className='todo-input' onChange={handleChange} ref={inputRef} />
+        <button className='todo-button'>Add a task</button>
         </form>
     )
 }
