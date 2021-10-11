f5afa17e902cbe535e07f6ea38d132499b86254a
Lewis Murray <lewisdmurray00@gmail.com>
2021-10-11 17:25:20 +0100

    Added a function allowing me to add a todo and it is stored into an array

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,6 +5,14 @@ import TodoForm from './TodoForm'
 function TodoList() {
     const[todos, settodos] = useState([])
 
+    const addTodo = todo =>{
+        if (!todo.text || /^\s*$/.test(todo.text)) {
+            return;
+          }
+          const newTodos = [todo, ...todos]
+
+          settodos(newTodos)
+        }
     return (
         <div>
             <h1>Enter your tasks!</h1>
