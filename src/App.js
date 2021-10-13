c4d507e221adf19ace560732be234d97d77376bb
Lewis Murray <lewisdmurray00@gmail.com>
2021-10-13 20:40:28 +0100

    Added all the states required

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import {useState, useEffect} from 'react';
 import TodoList from './components/TodoList';
 
 function App() {
+
+const [user,setUser] = useState('');
+const [email,setEmail] = useState('');
+const [password,setPassword] = useState('');
+const [emailError,setEmailError] = useState('');
+const [passwordError,setPasswordError] = useState('');
+const [hasAccount,setHasAccount] = useState(false);
+
   return (
     <div className="todo-app">
      <TodoList />
