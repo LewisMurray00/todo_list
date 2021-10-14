089df8c7a019a009466823952b55140ac36d228f
Lewis Murray <lewisdmurray00@gmail.com>
2021-10-14 22:53:35 +0100

    Added a handleSignup function with cases for errors

--- src/App.js
+++ src/App.js
@@ -13,6 +13,40 @@ const [emailError,setEmailError] = useState('');
 const [passwordError,setPasswordError] = useState('');
 const [hasAccount,setHasAccount] = useState(false);
 
+const handleLogin = () => {
+  app
+    .auth()
+    .signInWithEmailAndPassword(email,password)
+    .catch(err=>{
+      switch(err.code){
+        case "auth/invalid-email":
+        case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(err.message);
+            break;
+      }
+    });
+};
+
+const handleSignup = () => {
+  app
+  .auth()
+  .createUserWithEmailAndPassword(email, password)
+  .catch(err=>{
+    switch(err.code){
+      case "auth/email-already-in-use":
+      case "auth/invalid-email":
+          setEmailError(err.message);
+          break;
+        case "auth/weak-password":
+          setPasswordError(err.message);
+          break;
+    }
+  });
+}
 
   return (
     <div className="todo-app">
